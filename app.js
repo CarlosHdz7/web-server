0e38a06ad43f2ba13ba8554c662b9d0616d2bf3d
Carlos Eduardo <carlos.cabrera@udb.edu.sv>
2021-03-08 18:47:14 -0600

    Agregando template

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
+const port = 8080;
  
+// Middleware
+//Servir contenido estatico
+app.use( express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.sendFile(  __dirname + '/public/index.html')
 })
- 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo')
+
+app.get('/generic', (req, res) => {
+  res.sendFile(  __dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(  __dirname + '/public/elements.html')
 })
 
 app.get('/*', (req, res) => {
-    res.send('404 page not found')
+    res.sendFile(  __dirname + '/public/404.html')
 })
 
-app.listen(8080)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
